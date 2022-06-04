bfeb6120f507507afd385594b6b6cf61f78ef3b9
Naomi Cole <naomisarahcole@gmail.com>
2022-06-04 18:51:07 +1000

    Completed assignemnt

--- index.js
+++ index.js
@@ -3,4 +3,18 @@ var customerName = 'bob';
 
 function upperCaseCustomerName(){
     customerName = customerName.toUpperCase();
+};
+
+function setBestCustomer (){
+    bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+};
+
+const leastFavoriteCustomer = '';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Jake';
 }
